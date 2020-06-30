925bf3a6993d55f58c87bd9ad6952005a228f1fc
Rajkumar S B <thisisrajsb@gmail.com>
2020-06-30 12:48:07 +0530

    Add bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_secure_password
+  
   validates :email, uniqueness: true
   validates_format_of :email, with: /@/
   validates :password_digest, presence: true

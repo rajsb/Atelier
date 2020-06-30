c620a077b2a0977665cc187c0b877e4456d969e9
Rajkumar S B <thisisrajsb@gmail.com>
2020-06-30 19:17:11 +0530

    Add authenticable module

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -37,8 +37,13 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should update user' do
-    patch api_v1_user_url(@user), params: { user: { email: @user.email, password: '123456' } }, as: :json
+    patch api_v1_user_url(@user), params: { user: { email: @user.email, password: '123456' } }, headers: { Authorization: JsonWebToken.encode(user_id: @user.id) }, as: :json
     assert_response :success
+  end
+
+  test 'should forbid update user' do
+    patch api_v1_user_url(@user), params: { user: { email: @user.email } }, as: :json
+    assert_response :forbidden
   end
 
   test 'should not update user when invalid params are sent' do
@@ -49,8 +54,15 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
   # we respond with content of 204 which is No content
   test 'should destroy the user' do
     assert_difference('User.count', -1) do
-      delete api_v1_user_url(@user), as: :json
+      delete api_v1_user_url(@user), headers: { Authorization: JsonWebToken.encode(user_id: @user.id) }, as: :json
     end
     assert_response :no_content
+  end
+
+  test 'should forbid destroy user' do
+    assert_no_difference('User.count') do
+      delete api_v1_user_url(@user), as: :json
+    end
+    assert_response :forbidden
   end
 end

aaecd16359ec65d7baabaa9c04101d1dc4b80994
Rajkumar S B <thisisrajsb@gmail.com>
2020-06-30 17:15:49 +0530

    Setup jwt gem

---
+++ lib/json_web_token.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class JsonWebToken
+  SECRET_KEY = Rails.application.secrets.secret_key_base.to_s
+
+  def self.encode(payload, exp = 24.hours.from_now)
+    payload[:exp] = exp.to_i
+    JWT.encode(payload, SECRET_KEY)
+  end
+
+  def self.descode(token)
+    decoded = JWT.decode(token, SECRET_KEY).first
+
+    # This class is provided by rails. It allows us to retrieve a value of a Hash with a Symbol or String
+    HashWithIndifferentAccess.new decoded
+  end
+end
